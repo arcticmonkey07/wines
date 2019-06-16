68f4599f40c9a36519d890dd8c2c82b6c7098e95
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-06-16 13:15:31 +0300

    51 axios

--- src/components/Wine/Wines.jsx
+++ src/components/Wine/Wines.jsx
@@ -1,49 +1,16 @@
 import React from 'react';
 import './WineContainer.css';
 import Wine from "./Wine.jsx";
+import * as axios from "axios";
 
 const Wines = (props) => {
   if (props.wines.length === 0) {
-    props.setWines(
-        [
-          {
-            id: 1,
-            favorited: false,
-            image: 'http://quack-straw.surge.sh/img/Nine_Stones_Shiraz.jpg',
-            name: 'Nine Stones',
-            country: 'Australia',
-            grape: 'Shiraz',
-            sugar: 'dry',
-            color: 'Red',
-            price: 800,
-            description: 'Mclaren Valley. Чуть перечный, достаточно сочный, вишневый, насыщенный.'
-          },
-          {
-            id: 2,
-            favorited: false,
-            image: 'http://quack-straw.surge.sh/img/riesling_hagn.png',
-            name: 'Riesling Hagn',
-            country: 'Austria',
-            grape: 'Riesling',
-            sugar: 'dry',
-            color: 'White',
-            price: 713,
-            description: 'Изящный, полный, насыщенный рислинг.'
-          },
-          {
-            id: 3,
-            favorited: false,
-            image: 'http://quack-straw.surge.sh/img/Jinda-Lee_shiraz.jpg',
-            name: 'Jinda-Lee',
-            country: 'Australia',
-            grape: 'Shiraz',
-            sugar: 'dry',
-            color: 'Red',
-            price: 600,
-            description: 'Простое, легкое.'
-          },
-        ]
-    )
+
+    axios.get('http://localhost:3000/wines')
+         .then(response => {
+           props.setWines(response.data)
+         });
+
   }
 
   let winesElements = props.wines.map( w => <Wine favorite={props.favorite} unfavorite={props.unfavorite} id={w.id} favorited={w.favorited} image={w.image} name={w.name} country={w.country} grape={w.grape} sugar={w.sugar} color={w.color} price={w.price} description={w.description} key={w.id} />);
