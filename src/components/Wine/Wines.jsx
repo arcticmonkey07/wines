72cbcad92e71513162f11e1ca914216427a59153
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-06-18 13:06:04 +0300

    54 class, componentDidMount

--- src/components/Wine/Wines.jsx
+++ src/components/Wine/Wines.jsx
@@ -3,23 +3,22 @@ import './WineContainer.css';
 import Wine from "./Wine.jsx";
 import * as axios from "axios";
 
-const Wines = (props) => {
-  if (props.wines.length === 0) {
-
+class Wines extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/wines')
-         .then(response => {
-           props.setWines(response.data)
-         });
-
+        .then(response => {
+          this.props.setWines(response.data)
+        });
   }
 
-  let winesElements = props.wines.map( w => <Wine favorite={props.favorite} unfavorite={props.unfavorite} id={w.id} favorited={w.favorited} image={w.image} name={w.name} country={w.country} grape={w.grape} sugar={w.sugar} color={w.color} price={w.price} description={w.description} key={w.id} />);
-
-  return (
-      <div className='container'>
-        { winesElements }
-      </div>
-  )
+  render() {
+    return <div className='container'>
+      {
+        this.props.wines.map(w => <Wine favorite={this.props.favorite} unfavorite={this.props.unfavorite} id={w.id} favorited={w.favorited} image={w.image} name={w.name} country={w.country} grape={w.grape} sugar={w.sugar} color={w.color} price={w.price} description={w.description} key={w.id}/>)
+      }
+    </div>
+  }
 }
+
 
 export default Wines;
